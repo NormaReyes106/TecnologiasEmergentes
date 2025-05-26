5e06318e4fcbf7c02e1a5185c5de48c0dc917c6a
NormaReyes <norma.reyes.pina@gmail.com>
2025-05-25 23:52:25 -0600

    index.ts

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -2,7 +2,7 @@ import colors from 'colors';
 import server from "./server";
 
 
-const port = 3000;
+const port = process.env.port || 3000;
 server.listen(port, () => {
     console.log( colors.cyan.bold (`REST API en el puerto: ${port}`));
     console.log( colors.cyan.bold (`http://localhost:${port}`));
